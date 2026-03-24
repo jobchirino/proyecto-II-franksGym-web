6b5578615822cd7d256fce60811c03a213160678
Job <chirinosjob@gmail.com>
2026-03-24 17:37:09 -0400

    schedules activados

---
+++ src/app/api/athletes/especific-updates/route.js
@@ -0,0 +1,44 @@
+import { prisma } from "@/app/libs/prisma";
+import { NextResponse } from "next/server";
+
+export default async function POST( request ) {
+    const haceUnaSemana = new Date();
+    haceUnaSemana.setDate(haceUnaSemana.getDate() - 7);
+
+    const haceUnDia = new Date()
+    haceUnDia.setDate(haceUnDia.getDate() - 1)
+    try {
+        await prisma.athlete.updateMany({
+            where: {
+                isPaid: true,
+                membershipType: 'semanal',
+                lastPaymentDate: {
+                    lte: haceUnaSemana
+                }
+            },
+            
+            data: {
+                isPaid: false
+
+            }
+        });
+
+        await prisma.athlete.updateMany({
+            where: {
+                isPaid: true,
+                membershipType: 'por_dia',
+                lastPaymentDate: {
+                    lte: haceUnDia
+                }
+            },
+            
+            data: {
+                isPaid: false
+            }
+        })
+
+        return NextResponse.json({ message: 'Reseteo semanal exitoso', resultado }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Error al resetear la membresía semanal' }, { status: 500 });
+    }
+}
